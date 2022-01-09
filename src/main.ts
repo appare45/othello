63bbb817ebb27ebeb218a271863cabebbe55aa38
appare45 <64473501+appare45@users.noreply.github.com>
2022-01-10 01:59:14 +0900

    support window change

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import * as THREE from "three";
 import "./style.css";
 
-// レンダターを作成
-export const renderer = new THREE.WebGL1Renderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
 
-// カメラを作成
-// 引数は視野角、縦横比、描画の最短距離、描画の最長距離
-export const camera = new THREE.PerspectiveCamera(
+export const renderer = new THREE.WebGL1Renderer();
+export let camera = new THREE.PerspectiveCamera(
   45,
-  window.innerWidth / window.innerHeight,
+  windowWidth / windowHeight,
   1,
   500
 );
 camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
+
+// リサイズ時の処理
+const resize = () => {
+  renderer.setSize(window.innerWidth, windowHeight);
+  camera.aspect = windowWidth / windowHeight;
+};
+
+window.addEventListener("resize", resize);
+
+resize();
+document.body.appendChild(renderer.domElement);
 
 // シーンを作成
 export const scene = new THREE.Scene();
